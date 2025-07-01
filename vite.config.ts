a5dea2641c4d4d44e111a3858d2d94eba5c29a41
Mehvish Javed <mehvishj090@email.com>
2025-07-01 17:51:15 +0100

    fix: switched to vite.config.ts for clean build

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist' // ✅ this is required for Vercel
+  }
+})
